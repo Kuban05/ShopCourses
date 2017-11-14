571e137879144f24795387e1c072c8f8813aa46a
Micał Jakub <mical.jakub@gmail.com>
2017-11-14 13:49:06 +0100

    Create layout and view Index in controller home

---
+++ ShopCourses/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ShopCourses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopCourses.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Category> Category { get; set; }
+
+        public IEnumerable<Course> News { get; set; }
+
+        public IEnumerable<Course> Bestseller { get; set; }
+    }
+}
