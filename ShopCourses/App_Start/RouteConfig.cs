6d9df2603fc12d6ddfe40dd331b909e9a583fa5d
Micał Jakub <mical.jakub@gmail.com>
2017-11-14 14:33:20 +0100

    Add sites static and route for sites staic and add method to sites static in home controller

--- ShopCourses/App_Start/RouteConfig.cs
+++ ShopCourses/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace ShopCourses
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "SitesStatic",
+                url: "site/{name}.html",
+                defaults: new { controller = "Home", action = "SitesStatic" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- ShopCourses/Controllers/HomeController.cs
+++ ShopCourses/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ShopCourses.DAL;
 using ShopCourses.Models;
+using ShopCourses.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +16,23 @@ namespace ShopCourses.Controllers
         // GET: Home
         public ActionResult Index()
         {
-           
+            var category = db.Categories.OrderByDescending(o => o.NameCategory).ToList();
+            var news = db.Courses.Where(c => !c.Hidden).OrderBy(o => Guid.NewGuid()).Take(3).ToList();
+            var bestseller = db.Courses.Where(c => !c.Hidden && c.Bestseller).OrderBy(o => o.DateAdded).Take(3).ToList();
 
-            return View();
+            var vm = new HomeViewModel()
+            {
+                Category = category,
+                News = news,
+                Bestseller = bestseller
+            };
+
+            return View(vm);
+        }
+
+        public ActionResult SitesStatic(string name)
+        {
+            return View(name);
         }
     }
 }
